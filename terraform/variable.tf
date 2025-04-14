57bec763f1f115b29b8f93cf48d0b26d692ef1c0
satya-git07 <sbappadala@softility.com>
2025-04-14 15:48:17 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,11 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
-  default = "sekhar-452813"
+  default = "lyrical-bus-452711-c5"
 }
 
 variable "region" {
   description = "The region where the resources will be created"
   type        = string
-  default = "us-central1"
+  default = "us-west3-c"
 }
