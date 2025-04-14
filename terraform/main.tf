7c27cc89ccc9714dad3d8fdd4c461903066737e1
satya-git07 <sbappadala@softility.com>
2025-04-14 18:06:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "google" {
   region  = var.project_id
   zone    = var.region
